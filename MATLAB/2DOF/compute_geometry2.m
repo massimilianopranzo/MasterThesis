da8a7dd0315c21b9cddfceecb1cbf6b9eceafbaf
massimilianopranzo <massimiliano.pranzo@studenti.unitn.it>
2023-10-21 23:12:44 +0200

    Compute capacitance 2DOF

--- MATLAB/2DOF/compute_geometry2.m
+++ MATLAB/2DOF/compute_geometry2.m
@@ -1,25 +1,29 @@
 function [varargout] = compute_geometry2(t_f, varargin)
-    syms epsilon_1(x) epsilon_2(x) epsilon_3(x) w_0 tp_0 l_0 l_c epsilon_p epsilon_f xi xi_0
+    syms epsilon_1(x) epsilon_2(x) epsilon_3(x) w_0 tp_0 tf_0 l_0 l_c epsilon_p epsilon_f xi xi_0
     if length(varargin) == 1
         switch varargin{1}
             case "PSTRAIN"
                 pstrain.l = l_0 * (1 + epsilon_1(x));
-                pstrain.l_c = l_c;
-                pstrain.l_a 
-                pstrain.t_p = tp_0 * (1 + epsilon_3(x));
                 pstrain.w = w_0;
+                pstrain.t_p = tp_0 * (1 + epsilon_3(x));
+                pstrain.l_a = (l_0 - l_c) + (x - tf_0)^2 / (8 * (l_0 - l_c));
                 pstrain.A = pstrain.l * pstrain.w;
                 dA = pstrain.w * (1 + epsilon_1(x)); % * dxi
-                dCp = dA * epsilon_p / pstrain.t_p;
-                dCf = dA * epsilon_f / t_f;
-                pstrain.dC = ((2 / dCp) + (1 / dCf)) ^ (-1);
-                pstrain.C = int(pstrain.dC, xi);
-                pstrain.C = subs(pstrain.C, xi, xi_0 + l_0) - subs(pstrain.C, xi, xi_0);    
+
+                pstrain.Cp1 = pstrain.A * epsilon_p / tp_0;
+                pstrain.Cf1 = pstrain.A * epsilon_f / t_f(tf_0);
+                pstrain.C1 = ((2 / pstrain.Cp1) + (1 / pstrain.Cf1)) ^ (-1);
+
+                pstrain.dCp2 = dA * epsilon_p / pstrain.t_p;
+                pstrain.dCf2 = dA * epsilon_f / t_f(x);
+                pstrain.dC2 = ((2 / pstrain.dCp2) + (1 / pstrain.dCf2))^(-1);
+                pstrain.C2 = int(pstrain.dC2, xi);
+                pstrain.C2 = subs(pstrain.C2, xi, xi_0 + l_0) - subs(pstrain.C2, xi, xi_0);    
                 varargout{1} = pstrain;
             case "PSTRESS"
                 pstress.l = l_0 * (1 + epsilon_1(x));
-                pstress.t_p = tp_0;
                 pstress.w = w_0 * (1 + epsilon_2(x));
+                pstress.t_p = tp_0 * (1 + epsilon_3(x));
                 pstress.A = pstress.l * pstress.w;
                 dA = pstress.w * (1 + epsilon_1(x)); % * dxi
                 dCp = dA * epsilon_p / pstress.t_p;
@@ -45,15 +49,21 @@ function [varargout] = compute_geometry2(t_f, varargin)
         end    
     elseif isempty(varargin)
         pstrain.l = l_0 * (1 + epsilon_1(x));
-        pstrain.t_p = tp_0 * (1 + epsilon_3(x));
         pstrain.w = w_0;
+        pstrain.t_p = tp_0 * (1 + epsilon_3(x));
         pstrain.A = pstrain.l * pstrain.w;
         dA = pstrain.w * (1 + epsilon_1(x)); % * dxi
-        dCp = dA * epsilon_p / pstrain.t_p;
-        dCf = dA * epsilon_f / t_f;
-        pstrain.dC = ((2 / dCp) + (1 / dCf)) ^ (-1);
-        pstrain.C = int(pstrain.dC, xi);
-        pstrain.C = subs(pstrain.C, xi, xi_0 + l_0) - subs(pstrain.C, xi, xi_0);
+
+        pstrain.Cp1 = A * epsilon_p / tp_0;
+        pstrain.Cf1 = A * epsilon_f / t_f(t_f0);
+        pstrain.C1 = ((2 / Cp1) + (1 / Cf1)) ^ (-1);
+        
+        pstrain.dCp2 = dA * epsilon_p / pstrain.t_p;
+        pstrain.dCf2 = A * epsilon_f / t_f;
+        pstrain.dC2 = ((2 / dCp2) + (1 / dCf2))^(-1);
+        pstrain.C2 = int(pstrain.dC2, xi);
+        pstrain.C2 = subs(pstrain.C2, xi, xi_0 + l_0) - subs(pstrain.C2, xi, xi_0);    
+        varargout{1} = pstrain;
 
         pstress.l = l_0 * (1 + epsilon_1(x));
         pstress.t_p = tp_0;
@@ -65,6 +75,7 @@ function [varargout] = compute_geometry2(t_f, varargin)
         pstress.dC = ((2 / dCp) + (1 / dCf)) ^ (-1);
         pstress.C = int(pstress.dC, xi);
         pstress.C = subs(pstress.C, xi, xi_0 + l_0) - subs(pstress.C, xi, xi_0);
+        varargout{2} = pstress;
 
         nostrain.l = l_0;
         nostrain.t_p = tp_0;
@@ -76,9 +87,6 @@ function [varargout] = compute_geometry2(t_f, varargin)
         nostrain.dC = ((2 / dCp) + (1 / dCf)) ^ (-1);
         nostrain.C = int(nostrain.dC, xi);
         nostrain.C = subs(nostrain.C, xi, xi_0 + l_0) - subs(nostrain.C, xi, xi_0);
-
-        varargout{1} = pstrain;
-        varargout{2} = pstress;
         varargout{3} = nostrain;
     else
         error("Invalid number of input arguments")
--- MATLAB/Functions and Scripts/makeassumptions.m
+++ MATLAB/Functions and Scripts/makeassumptions.m
@@ -1,16 +1,16 @@
-assume(nu,'real'); assumeAlso(nu>0); 
-assume(Y_p,'real'); assumeAlso(Y_p>0);
-assume(epsilon_p,'real'); assumeAlso(epsilon_p>0);
-assume(epsilon_f,'real'); assumeAlso(epsilon_f>0);
-assume(EBD_p,'real'); assumeAlso(EBD_p>0);
-assume(EBD_f,'real'); assumeAlso(EBD_f>0);
-assume(x,'real'); assumeAlso(x>0);
-assume(l_0,'real'); assumeAlso(l_0>0);
-assume(w_0,'real'); assumeAlso(w_0>0);
-assume(tp_0,'real'); assumeAlso(tp_0>0);
-assume(xi,'real'); assumeAlso(xi>0);
-assume(xi_0,'real'); assumeAlso(xi_0>0);
-assume(tf_0,'real'); assumeAlso(tf_0>0);
+assume(nu,'real'); assumeAlso(nu,'positive'); assumeAlso(nu~=0);
+assume(Y_p,'real'); assumeAlso(Y_p,'positive'); assumeAlso(Y_p~=0);
+assume(epsilon_p,'real'); assumeAlso(epsilon_p,'positive'); assumeAlso(epsilon_p~=0);
+assume(epsilon_f,'real'); assumeAlso(epsilon_f,'positive'); assumeAlso(epsilon_f~=0);
+assume(EBD_p,'real'); assumeAlso(EBD_p,'positive'); assumeAlso(EBD_p~=0);
+assume(EBD_f,'real'); assumeAlso(EBD_f,'positive'); assumeAlso(EBD_f~=0);
+assume(x,'real'); assumeAlso(x,'positive'); assumeAlso(x~=0);
+assume(l_0,'real'); assumeAlso(l_0,'positive'); assumeAlso(l_0~=0);
+assume(w_0,'real'); assumeAlso(w_0,'positive'); assumeAlso(w_0~=0); 
+assume(tp_0,'real'); assumeAlso(tp_0,'positive'); assumeAlso(tp_0~=0);
+assume(xi,'real'); assumeAlso(xi,'positive'); assumeAlso(xi~=0);
+assume(xi_0,'real'); assumeAlso(xi_0,'positive'); assumeAlso(xi_0~=0);
+assume(tf_0,'real'); assumeAlso(tf_0,'positive'); assumeAlso(tf_0~=0);
 assumeAlso(epsilon_p * EBD_p, "real")
 assumeAlso(epsilon_p * EBD_p, "positive")
 assumeAlso(epsilon_f * EBD_f, "real")
